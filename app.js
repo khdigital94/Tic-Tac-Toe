e2653e9d2968e2e49e12cce8a4815f91cf5c95b9
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-02 22:49:49 +0200

    Making the game logic work in the console

--- app.js
+++ app.js
@@ -1,81 +1,86 @@
-// At first i need to create a game board. The board could be created by using an array
-// Then I have to create a factory function to create the player. It should include a name, score and id
-// I need a function to check which players turn it is 
+const createGameBoard = (function () {
+	let gameBoard = [
+		["", "", ""],
+		["", "", ""],
+		["", "", ""],
+	];
 
-
-// Wie könnte ein Spieler einen Zug machen?
-// 1. Eine Funktion erstellen
-// 2. Die Funktion nimmt einen Parameter entgegen. Der Parameter ist das gewählte Feld.
-// 3. Das Feld erhält ein x oder o, je nachdem welcher spieler am zug ist.
-
-function createBoard() {
-    const rows = 3;
-    const cols = 3;
-    let gameBoard = [["", "", ""], ["", "", ""],["", "", ""]]
-    for (let i = 0; i < gameBoard.length; i++) {
-        console.log(gameBoard[i])
-    }
-}
+	return gameBoard;
+})();
 
 // Creates the player
 function createPlayer(name, symbol) {
-    let score = 0;
-    const id = crypto.randomUUID();
+	let score = 0;
 
-    const increaseScore = () => {
-        return score++
-    }
+	const increaseScore = () => {
+		return score++;
+	};
 
-    const getScore = () => {
-        return score;
-    }
+	const getScore = () => {
+		return score;
+	};
 
-    return {
-        name,
-        symbol, 
-        score, 
-        id, 
-        increaseScore,
-        getScore,
-    }
-}
-
-function checkWinner(board) {
-    const combinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    for (let comb of Winningcombinations) {
-        const [a, b, c] = combo;
-
-         if (
-            board[a] &&
-            board[a] == board[b] &&
-            board[b] == board[c]
-         ) {
-            return board[a];
-         }
-    }
-
-    if (board.every(cell => cell)) {
-        return 'Draw'
-    }
-
-    return null;
+	return {
+		name,
+		symbol,
+		score,
+		increaseScore,
+		getScore,
+	};
 }
 
 const player1 = createPlayer("Kevin", "x");
-const player2 = createPlayer("Markus", "y");
-const gameController = createGameController();
+const player2 = createPlayer("Markus", "o");
+let activePlayer = player1;
+const gameBoard = createGameBoard;
 
-createBoard();
+// Checks for winner
+function checkWinner(board) {
+	const combinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	board = board.flat();
 
-gameController.setMarker(2);
-gameController.refreshBoard();
+	for (let comb of combinations) {
+		const [a, b, c] = comb;
+
+		if (board[a] && board[a] == board[b] && board[b] == board[c]) {
+			console.log(`Spieler ${activePlayer.name} hat gewonnen!`);
+			break;
+		}
+	}
+
+	if (board.every((cell) => cell)) {
+		return "Draw";
+	}
+
+	return null;
+}
+
+// Plays Round
+function playRound(row, col) {
+	board = gameBoard;
+
+	if (board[row][col] !== "") {
+		console.log("Das Feld ist bereits belegt!");
+		return;
+	}
+
+	board[row][col] = activePlayer.symbol;
+
+	checkWinner(board, activePlayer);
+	activePlayer = activePlayer === player1 ? player2 : player1;
+}
+
+playRound(0, 0);
+playRound(1, 1);
+playRound(0, 1);
+playRound(2, 1);
+playRound(0, 2);
