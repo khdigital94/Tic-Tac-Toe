75be0f2fae30bb0618c0c133c838c442e7c27454
Kevin Henrich <153145008+khdigital94@users.noreply.github.com>
2025-04-03 00:01:37 +0200

    Adding more functionality to the game
    The game now creates an UI that can be used to play
    instead of playing the game entirely in the console.

--- app.js
+++ app.js
@@ -1,86 +1,120 @@
-const createGameBoard = (function () {
-	let gameBoard = [
-		["", "", ""],
-		["", "", ""],
-		["", "", ""],
-	];
+document.addEventListener("DOMContentLoaded", () => {
+	const gameBoard = (function () {
+		let board = [
+			["", "", ""],
+			["", "", ""],
+			["", "", ""],
+		];
 
-	return gameBoard;
-})();
+		const render = () => {
+			const appContainer = document.querySelector("#gameBoard");
+			appContainer.innerHTML = "";
 
-// Creates the player
-function createPlayer(name, symbol) {
-	let score = 0;
+			board.forEach((row, rowIndex) => {
+				row.forEach((col, colIndex) => {
+					const cellElement = document.createElement("div");
+					cellElement.classList.add("field");
+					cellElement.dataset.row = rowIndex;
+					cellElement.dataset.col = colIndex;
 
-	const increaseScore = () => {
-		return score++;
-	};
+					cellElement.addEventListener("click", () => playRound(rowIndex, colIndex));
+					cellElement.textContent = board[rowIndex][colIndex];
 
-	const getScore = () => {
-		return score;
-	};
+					appContainer.appendChild(cellElement);
+				});
+			});
+		};
 
-	return {
-		name,
-		symbol,
-		score,
-		increaseScore,
-		getScore,
-	};
-}
+		const clear = () => {
+			for (let i = 0; i < board.length; i++) {
+				for (let j = 0; j < board[i].length; j++) {
+					board[i][j] = "";
+				}
+			}
+			render();
+		};
 
-const player1 = createPlayer("Kevin", "x");
-const player2 = createPlayer("Markus", "o");
-let activePlayer = player1;
-const gameBoard = createGameBoard;
+		const getBoard = () => board;
 
-// Checks for winner
-function checkWinner(board) {
-	const combinations = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	board = board.flat();
+		const updateMessage = (message) => {
+			const messageContainer = document.querySelector("#statusMessage");
+			messageContainer.textContent = message;
+		};
 
-	for (let comb of combinations) {
-		const [a, b, c] = comb;
+		return { render, clear, getBoard, updateMessage };
+	})();
 
-		if (board[a] && board[a] == board[b] && board[b] == board[c]) {
-			console.log(`Spieler ${activePlayer.name} hat gewonnen!`);
-			break;
+	function createPlayer(name, symbol) {
+		let score = 0;
+
+		const increaseScore = () => score++;
+		const getScore = () => score;
+
+		return {
+			name,
+			symbol,
+			increaseScore,
+			getScore,
+		};
+	}
+
+	const player1 = createPlayer("Kevin", "x");
+	const player2 = createPlayer("Markus", "o");
+	let activePlayer = player1;
+
+	function checkWinner(board) {
+		const combinations = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		const flatBoard = board.flat();
+
+		for (let comb of combinations) {
+			const [a, b, c] = comb;
+			if (flatBoard[a] && flatBoard[a] === flatBoard[b] && flatBoard[b] === flatBoard[c]) {
+				gameBoard.updateMessage(`${activePlayer.name} hat diese Runde gewonnen!`);
+				activePlayer.increaseScore;
+				setTimeout(() => {
+					gameBoard.clear();
+					gameBoard.updateMessage("");
+				}, 1000);
+				return;
+			}
+		}
+
+		if (flatBoard.every((cell) => cell)) {
+			gameBoard.updateMessage("Unentschieden!");
+			setTimeout(() => {
+				gameBoard.clear();
+				gameBoard.updateMessage("");
+			}, 1000);
 		}
 	}
 
-	if (board.every((cell) => cell)) {
-		return "Draw";
+	function playRound(row, col) {
+		const board = gameBoard.getBoard();
+
+		if (board[row][col] !== "") {
+			gameBoard.updateMessage("Dieses Feld ist bereits belegt!");
+			return;
+		}
+
+		board[row][col] = activePlayer.symbol;
+
+		checkWinner(board);
+
+		activePlayer = activePlayer === player1 ? player2 : player1;
+		gameBoard.updateMessage(`${activePlayer.name} ist am Zug!`);
+		gameBoard.render();
 	}
 
-	return null;
-}
-
-// Plays Round
-function playRound(row, col) {
-	board = gameBoard;
-
-	if (board[row][col] !== "") {
-		console.log("Das Feld ist bereits belegt!");
-		return;
-	}
-
-	board[row][col] = activePlayer.symbol;
-
-	checkWinner(board, activePlayer);
-	activePlayer = activePlayer === player1 ? player2 : player1;
-}
-
-playRound(0, 0);
-playRound(1, 1);
-playRound(0, 1);
-playRound(2, 1);
-playRound(0, 2);
+	gameBoard.render();
+	gameBoard.updateMessage(`${player1.name} ist am Zug!`);
+});
